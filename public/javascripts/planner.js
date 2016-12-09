52a22f7cb1fb8a1af1897d15249491d0c6783c0e
Gloria Budiman <wahyu.g@gmail.com>
2016-12-09 04:16:34 -0800

    Implemented shortest polyline intersection.

--- public/javascripts/planner.js
+++ public/javascripts/planner.js
@@ -49,7 +49,10 @@ function test_routing() {
                     travelMode: 'driving'
                   })
 
-                  test_intersect(r_results);
+                  test_intersect(r_results, 'Los Angeles, CA');
+                  test_intersect(r_results, 'Topanga, CA');
+                  test_intersect(r_results, 'Monrovia, CA');
+                  test_intersect(r_results, 'Riverside, CA');
                 }
               })
             } else {
@@ -64,18 +67,96 @@ function test_routing() {
   })
 }
 
-function test_intersect(r) {
-  console.log(r);
+function test_intersect(r, x) {
+  test_place_marker(x).then(function(marker) {
+    find_shortest_intersection(r, marker).then(function(trig) {
+      console.log(' ======= Shortest intersection from ' + x + ': ' + trig.distance);
+      draw_triangle(trig, marker);
+    });
+  })
+  //test_haversine();
 
-  test_haversine();
+}
+
+function draw_triangle(trig, marker) {
+  path = new Array();
+
+  
+  path.push([trig.stop.lat(), trig.stop.lng()]);
+  path.push([marker.latlng.lat(), marker.latlng.lng()]);
+
+  if (trig.start != undefined) {
+    path.push([trig.start.lat(), trig.start.lng()]);
+    path.push([marker.latlng.lat(), marker.latlng.lng()]);
+  }
+
+  map.drawPolyline({
+    path: path
+  })
+}
+
+function find_shortest_intersection(r, marker) {
+  var min_distance = Number.MAX_SAFE_INTEGER;
+  var min_start, min_stop;
+  var latched_distance;
+
+  var expand_latlng = function(x) {
+    return [x.lat(), x.lng()];
+  }
+
+  var expand_point = function(x) {
+    return get_distance(x, marker.latlng);
+  }
+
+  var update_min = function(start, stop, _dist) {
+    if (latched_distance == undefined) {
+      dist = _dist;
+    } else {
+      dist = (latched_distance + _dist) / 2;
+    }
+
+    latched_distance = _dist;
+
+    if (dist < min_distance) {
+      min_distance = dist;
+      min_start = start;
+      min_stop = stop;
+    }
+  }
+
+  return new Promise(
+    function(resolve, reject) {
+      $.each(r, function(h, route) {
+        $.each(route.legs, function(i, leg) {
+          var step_count = leg.steps.length;
+
+          $.each(leg.steps, function(j, step) {
+            switch(j) {
+              case 0:
+                console.log(expand_latlng(step.start_location) + ': ' + expand_point(step.start_location));
+                update_min(null, step.start_location, expand_point(step.start_location));
+              default:
+                console.log(expand_latlng(step.end_location) + ': ' + expand_point(step.end_location));
+                update_min(step.start_location, step.end_location, expand_point(step.end_location));
+                break;
+            }
+          })
+        })
+
+        resolve({
+          start: min_start,
+          stop: min_stop,
+          distance: min_distance
+        })
+      })
+    }
+  );
 }
 
 function test_place_marker(x) {
   return new Promise(
     function(resolve, reject) {
       place_marker_on_address(x).then(function(latlng) {
-        console.log('Marker placed at ' + latlng);
-
         resolve({
           address: x,
           latlng: latlng
